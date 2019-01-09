ae91cf219bf2db87b8a9e54584a633cd2cc57842
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-01-09 10:39:14 -0500

    Sort out the Template mess

--- src/main.rb
+++ src/main.rb
@@ -1,6 +1,5 @@
-load "template.rb"
 load "instruction.rb"
 load "table.rb"
 load "parser.rb"
 
-puts Parser.new(read_table(STDIN), {}, Template::NONE).generate
+puts Parser.new(read_table(STDIN), {}, 0).generate
--- src/parser.rb
+++ src/parser.rb
@@ -3,20 +3,20 @@ class Parser
 
   def initialize(instructions, settings, parent_common)
     @settings = settings
-    @common = Template::ALL.exclude(parent_common || Template::NONE)
+    @common = ~0 ^ (parent_common || 0)
     instructions.each do |instr|
-      @common = instr.template.common(@common)
+      @common &= instr.template
     end
     if instructions.length <= 1
       @children = [nil]
       return
     end
-    if @common === Template::NONE
+    if @common == 0
       throw AmbiguousCases.new
     end
     children = {}
     instructions.each do |instr|
-      bits = instr.bits(@common).bits
+      bits = instr.bits & @common
       group = children[bits]
       if (group)
         group.push(instr)
@@ -53,16 +53,16 @@ class Parser
   end
 
   def gen_switch_arg
-    "#{gen_arg}&0x#{@common.bits.to_s(16)}"
+    "#{gen_arg}&0x#{@common.to_s(16)}"
   end
 
   def gen_child_cases
     @children.map { |bits, child|
-      "case 0x#{(bits & @common.bits).to_s(16)}: #{child.send(:print_child)}; break;" }.join
+      "case 0x#{(bits & @common).to_s(16)}: #{child.send(:print_child)}; break;" }.join
   end
 
   def gen_return
-    "return 0x#{@common.bits.to_s(16)}"
+    "return 0x#{@common.to_s(16)}"
   end
 
   def gen_default
